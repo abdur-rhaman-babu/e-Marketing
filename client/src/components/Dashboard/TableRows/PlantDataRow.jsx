2884bd8f363c7e4356d7c6d0235fdf9f7d50e347
abdur rhaman babu <abdulaziz139953@gmail.com>
2025-03-19 23:42:05 +0600

    delete product by seller form db

--- client/src/components/Dashboard/TableRows/PlantDataRow.jsx
+++ client/src/components/Dashboard/TableRows/PlantDataRow.jsx
@@ -1,72 +1,91 @@
-import { useState } from 'react'
-import DeleteModal from '../../Modal/DeleteModal'
-import UpdatePlantModal from '../../Modal/UpdatePlantModal'
-import PropTypes from 'prop-types'
+import { useState } from "react";
+import DeleteModal from "../../Modal/DeleteModal";
+import UpdatePlantModal from "../../Modal/UpdatePlantModal";
+import PropTypes from "prop-types";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
 
-const PlantDataRow = ({product}) => {
-  let [isOpen, setIsOpen] = useState(false)
-  const [isEditModalOpen, setIsEditModalOpen] = useState(false)
+const PlantDataRow = ({ product, refetch }) => {
+  const axiosSecure = useAxiosSecure();
+  let [isOpen, setIsOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
   function openModal() {
-    setIsOpen(true)
+    setIsOpen(true);
   }
   function closeModal() {
-    setIsOpen(false)
+    setIsOpen(false);
   }
 
-  const {image, name, category, quantity, price, _id} = product || {}
+  const { image, name, category, quantity, price, _id } = product || {};
+
+  const handleDeleteProduct = async () => {
+    try {
+      await axiosSecure.delete(`/product/${_id}`);
+      toast.success('Product delete successfully!')
+      refetch()
+    } catch (err) {
+      toast.error(err.response.data);
+    } finally {
+      closeModal();
+    }
+  };
 
   return (
     <tr>
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <div className='flex items-center'>
-          <div className='flex-shrink-0'>
-            <div className='block relative'>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <div className="flex items-center">
+          <div className="flex-shrink-0">
+            <div className="block relative">
               <img
-                alt='profile'
+                alt="profile"
                 src={image}
-                className='mx-auto object-cover rounded h-10 w-15 '
+                className="mx-auto object-cover rounded h-10 w-15 "
               />
             </div>
           </div>
         </div>
       </td>
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <p className='text-gray-900 whitespace-no-wrap'>{name}</p>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">{name}</p>
       </td>
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <p className='text-gray-900 whitespace-no-wrap'>{category}</p>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">{category}</p>
       </td>
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <p className='text-gray-900 whitespace-no-wrap'>${price}</p>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">${price}</p>
       </td>
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <p className='text-gray-900 whitespace-no-wrap'>{quantity}</p>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">{quantity}</p>
       </td>
 
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
         <span
           onClick={openModal}
-          className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight'
+          className="relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight"
         >
           <span
-            aria-hidden='true'
-            className='absolute inset-0 bg-red-200 opacity-50 rounded-full'
+            aria-hidden="true"
+            className="absolute inset-0 bg-red-200 opacity-50 rounded-full"
           ></span>
-          <span className='relative'>Delete</span>
+          <span className="relative">Delete</span>
         </span>
-        <DeleteModal isOpen={isOpen} closeModal={closeModal} />
+        <DeleteModal
+          isOpen={isOpen}
+          closeModal={closeModal}
+          handleDeleteProduct={handleDeleteProduct}
+        />
       </td>
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
         <span
           onClick={() => setIsEditModalOpen(true)}
-          className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight'
+          className="relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight"
         >
           <span
-            aria-hidden='true'
-            className='absolute inset-0 bg-green-200 opacity-50 rounded-full'
+            aria-hidden="true"
+            className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
           ></span>
-          <span className='relative'>Update</span>
+          <span className="relative">Update</span>
         </span>
         <UpdatePlantModal
           isOpen={isEditModalOpen}
@@ -74,12 +93,12 @@ const PlantDataRow = ({product}) => {
         />
       </td>
     </tr>
-  )
-}
+  );
+};
 
 PlantDataRow.propTypes = {
   product: PropTypes.object,
   refetch: PropTypes.func,
 };
 
-export default PlantDataRow
+export default PlantDataRow;
